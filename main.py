407253da0c64d3006ff6e03b46291f5b7c123810
Imanuel <charliechatur@gmail.com>
2025-03-10 16:41:59 -0400

    Added write methods and made it all work

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import json
 import sqlite3
+import csv
+from Customer import Customer
 
 def create_connection():
     try:
@@ -13,6 +16,34 @@ def create_cursor(conn):
     except Exception as e:
         print(e)
 
+def reset_db(conn, cursor):
+    cursor.execute('DELETE FROM Customers_Imanuel')
+    cursor.execute('DELETE FROM Items_Imanuel')
+    cursor.execute('DELETE FROM Transactions_Imanuel')
+    conn.commit()
+
+def write_csv_to_db(conn, cursor):
+    r = csv.reader(open('Customers_Imanuel.csv'), delimiter='-')
+    customers = [cust for cust in r]
+    print(customers)
+    sql = "INSERT INTO Customers_Imanuel VALUES (?, ?, ?, ?)"
+    cursor.executemany(sql, customers)
+    conn.commit()
+    print("Customer CSV Inserted successfully")
+
+def write_json_to_db(conn, cursor):
+    with open('Items_Imanuel.json', 'r') as f:
+        loader = json.load(f)
+        items = [[i['iid'], i['name'], i['category'],i['price']] for i in loader]
+        sql = "INSERT INTO Items_Imanuel VALUES (?, ?, ?, ?)"
+        cursor.executemany(sql, items)
+        conn.commit()
+        print("Item json Inserted successfully")
+
+def write_file_to_db(conn, cursor):
+    with open('Transactions_Imanuel.txt.csv', 'r') as f:
+        pass
+
 def main():
     conn = create_connection()
     if conn is None:
@@ -24,8 +55,10 @@ def main():
         print("Unable to Connect to File.")
         return
 
-
+    reset_db(conn, c)
+    write_csv_to_db(conn, c)
+    write_json_to_db(conn, c)
 
 if __name__ == '__main__':
     main()
-    print("Program Complete")
+    print("Safe Exit :)")
