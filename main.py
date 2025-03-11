009083ed3061415fda2836965aee3fa2cd7c1448
Imanuel <charliechatur@gmail.com>
2025-03-11 12:58:54 -0400

    Formatting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sqlite3
 import csv
 from Customer import Customer
 
+
 def create_connection():
     try:
         return sqlite3.connect('MyStore_Imanuel.db')
@@ -10,17 +11,20 @@ def create_connection():
     except sqlite3.Error as e:
         print(e)
 
+
 def create_cursor(conn):
     try:
         return conn.cursor()
     except Exception as e:
         print(e)
 
+
 def reset_db(conn, cursor):
     cursor.execute('DELETE FROM Customers_Imanuel')
     cursor.execute('DELETE FROM Items_Imanuel')
     cursor.execute('DELETE FROM Transactions_Imanuel')
     conn.commit()
+
 
 def write_csv_to_db(conn, cursor):
     r = csv.reader(open('Customers_Imanuel.csv'), delimiter='-')
@@ -31,6 +35,7 @@ def write_csv_to_db(conn, cursor):
     conn.commit()
     print("Customer CSV Inserted successfully")
 
+
 def write_json_to_db(conn, cursor):
     with open('Items_Imanuel.json', 'r') as f:
         loader = json.load(f)
@@ -39,6 +44,7 @@ def write_json_to_db(conn, cursor):
         cursor.executemany(sql, items)
         conn.commit()
         print("Item json Inserted successfully")
+
 
 def write_file_to_db(conn, cursor):
     with open('Transactions_Imanuel.txt', 'r') as f:
@@ -49,6 +55,8 @@ def write_file_to_db(conn, cursor):
         sql = "INSERT INTO Transactions_Imanuel VALUES (?, ?, ?, ?)"
         cursor.executemany(sql, transactions)
         conn.commit()
+
+
 def main():
     conn = create_connection()
     if conn is None:
@@ -64,6 +72,7 @@ def main():
     write_csv_to_db(conn, c)
     write_json_to_db(conn, c)
     write_file_to_db(conn, c)
+
 
 if __name__ == '__main__':
     main()
